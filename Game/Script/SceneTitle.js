80d77c2dbffa48321e5ca351d9e860f05f100475
pupungeggang2 <kyongkyong2892@gmail.com>
2024-12-18 04:36:38 +0900

    0.0.0:Display Test

---
+++ Game/Script/SceneTitle.js
@@ -0,0 +1,19 @@
+function loopTitle() {
+    displayTitle()
+}
+
+function displayTitle() {
+    drawSceneInit()
+
+    context.fillText(`Ceramic Defense`, UI.title.textTitle[0], UI.title.textTitle[1])
+}
+
+function mouseUpTitle(x, y, button) {
+    if (button === 0) {
+        if (menu === false) {
+            if (state === '') {
+                
+            }
+        }
+    }
+}
